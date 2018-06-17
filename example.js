9027a46ff9333dcfa0e638e9b41570c5c346675c
oliphunt <rich.brear@gmail.com>
2018-06-17 20:16:10 +0100

    Add a namespace.

--- example.js
+++ example.js
@@ -5,16 +5,16 @@ circle_2_y = circle_y;
 var speed = 2;
 
 function loop(){
-		cls();
-		rectangle(circle_x);
-		line(circle_x, 0, 0, 600);
-		line(600, 0, 600, 600);
-		circle(circle_x, circle_y, circle_diameter, 'red');
+		Ombalom.cls();
+		Ombalom.rectangle(circle_x);
+		Ombalom.line(circle_x, 0, 0, 600);
+		Ombalom.line(600, 0, 600, 600);
+		Ombalom.circle(circle_x, circle_y, circle_diameter, 'red');
 		circle_x = circle_x + speed;
 		circle_diameter = circle_diameter - (speed /2);
 		circle_y = circle_y;
 		circle_2_y = circle_2_y + (speed / 2)
-		circle(circle_x, circle_2_y, circle_diameter, 'red');
+		Ombalom.circle(circle_x, circle_2_y, circle_diameter, 'red');
 		circle_x = circle_x + speed;
 		circle_diameter = circle_diameter - (speed /2);
 		circle_y = circle_y + (speed / 4);
@@ -26,4 +26,4 @@ function loop(){
 		}
 }
 
-start();
+Ombalom.start();
